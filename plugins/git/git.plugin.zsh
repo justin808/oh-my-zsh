cdc4f58147e472706c1f471da8b825342343fb06
Justin Gordon <justin.gordon@gmail.com>
2013-09-29 15:12:08 -1000

    Don't alias g, gl

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,14 +1,14 @@
 # Aliases
-alias g='git'
-compdef g=git
+# alias g='git'
+# compdef g=git
 alias gst='git status'
 compdef _git gst=git-status
 alias gd='git diff'
 compdef _git gd=git-diff
 alias gdc='git diff --cached'
 compdef _git gdc=git-diff
-alias gl='git pull'
-compdef _git gl=git-pull
+# alias gl='git pull'
+# compdef _git gl=git-pull
 alias gup='git pull --rebase'
 compdef _git gup=git-fetch
 alias gp='git push'
